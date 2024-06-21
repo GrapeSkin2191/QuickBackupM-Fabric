284237a05bd8d0fd3d60b31dc1e8dc7fab68abf9
SAZrX <107967223+GrapeSkin2191@users.noreply.github.com>
2024-06-22 00:29:16 +0800

    Update QbmManager.java
    to make backuplist ordered by time

--- src/main/java/dev/skydynamic/quickbackupmulti/utils/QbmManager.java
+++ src/main/java/dev/skydynamic/quickbackupmulti/utils/QbmManager.java
@@ -183,6 +183,17 @@ public class QbmManager {
     }
 
     public static List<String> getBackupsList() {
+        List<File> fileList = new ArrayList<>(List.of(Objects.requireNonNull(getBackupDir().toFile().listFiles())));
+        fileList.sort((o1, o2) -> {
+            BackupInfo backupInfo1 = getDataBase().getSlotInfo(o1.getName());
+            BackupInfo backupInfo2 = getDataBase().getSlotInfo(o2.getName());
+            if (backupInfo1 == null) return 1;
+            if (backupInfo2 == null) return -1;
+            long diff = backupInfo1.getTimestamp() - backupInfo2.getTimestamp();
+            if (diff > 0) return 1;
+            else if (diff < 0) return -1;
+            else return 0;
+        });
         List<String> backupsDirList = new ArrayList<>();
         for (File file : Objects.requireNonNull(getBackupDir().toFile().listFiles())) {
             // if (file.isDirectory() && backupDir.resolve(file.getName()).toFile().exists() && backupDir.resolve(file.getName() + "_info.json").toFile().exists()) {
